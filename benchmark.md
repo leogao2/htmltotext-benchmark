25075e707951a49a8b5145412c48a0ec3a588c26
Leo Gao <leogao31@gmail.com>
2020-08-06 08:13:05 -0600

    Add table

---
+++ benchmark.md
@@ -0,0 +1,6 @@
+# Benchmarks
+|Algorithm|Word IOU(H)|DiffWords(L)|
+|---------|-----------|------------|
+|Newspaper|     0.5831|      0.4372|
+|JusText  |**0.6269** |**0.3709**  |
+|DoNothing|     0.2903|      0.8044|
--- benchmarker.py
+++ benchmarker.py
@@ -1,5 +1,7 @@
 from algorithms import algorithms
 from metrics import metrics
+	
+from pytablewriter import MarkdownTableWriter
 
 import os
 
@@ -17,7 +19,10 @@ def get_documents_for(lang):
         with open('benchmarkdata/' + lang + '/html/' + doc) as fh, open('benchmarkdata/' + lang + '/text/' + doc[:-4] + 'txt') as fh2:
             yield fh.read(), fh2.read()
 
+values = []
+
 for algo in algorithms:
+    row = [algo.name()]
     for metric in metrics:
         scores = []
         for html, true in get_documents_for('en'):
@@ -25,4 +30,23 @@ for algo in algorithms:
             score = metric.distance(true, extract)
             scores.append(score)
 
+        row.append(mean(scores))
         print(algo.name(), metric.name(), mean(scores))
+    values.append(row)
+
+# bold max value in each column
+for col in range(1, len(metrics) + 1):
+    coldata = list(map(lambda x: x[col], values))
+    maxval = min(coldata) if metrics[col - 1].lower_is_better() else max(coldata)
+    for row in range(len(values)):
+        if values[row][col] == maxval:
+            values[row][col] = '**{:.4f}**'.format(maxval)
+
+# write table
+writer = MarkdownTableWriter()
+writer.table_name = "Benchmarks ({})".format('en')
+writer.headers = ["Algorithm"] + list(map(lambda x: x.name() + '(' + ('L' if x.lower_is_better() else 'H') + ')', metrics))
+writer.value_matrix = values
+
+with open('benchmark.md', 'w') as fh:
+    fh.write(writer.dumps())
