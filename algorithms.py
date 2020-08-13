c2cc2a195eb69769cf1e3c20e94887b06142b333
Leo Gao <leogao31@gmail.com>
2020-08-12 23:49:24 -0600

    Implement a bunch more algorithms

--- algorithms.py
+++ algorithms.py
@@ -11,7 +11,7 @@ class Algorithm(ABC):
 
 
 from newspaper import Article
-class Newspaper(ABC):
+class Newspaper(Algorithm):
     def name(self):
         return "Newspaper"
 
@@ -22,13 +22,43 @@ class Newspaper(ABC):
 
         return article.text
 
+
 from justext import justext, get_stoplist
-class JusText(ABC):
+class JusText(Algorithm):
     def name(self):
         return "JusText"
 
     def extract(self, html):
         return '\n\n'.join([x.text for x in justext(html, get_stoplist('English')) if not x.is_boilerplate])
+
+
+import trafilatura
+class Trafilatura(Algorithm):
+    def name(self):
+        return "Trafilatura"
+
+    def extract(self, html):
+        return trafilatura.extract(html)
+
+
+from goose3 import Goose
+class Goose3(Algorithm):
+    def name(self):
+        return "Goose3"
+
+    def extract(self, html):
+        g = Goose()
+        return g.extract(raw_html=html).cleaned_text
+
+
+import dragnet
+class DragNet(Algorithm):
+    def name(self):
+        return "DragNet"
+
+    def extract(self, html):
+        return dragnet.extract_content_and_comments(html)
+
 
 
 class DoNothing(ABC):
@@ -42,5 +72,8 @@ class DoNothing(ABC):
 algorithms = [
     Newspaper(),
     JusText(),
+    Trafilatura(),
+    Goose3(),
+    DragNet(),
     DoNothing(),
 ]
