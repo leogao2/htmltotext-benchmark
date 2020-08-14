406ec58917d12e719b8949e23baea7c447fbe5f8
Leo Gao <leogao31@gmail.com>
2020-08-14 13:59:29 -0600

    Spoof user agent

--- labelling_helper.py
+++ labelling_helper.py
@@ -4,13 +4,13 @@ import os
 import shutil
 
 
-lang = 'de'
+lang = 'zh'
 
 while True:
     url = input().strip()
     slug = input().strip()
 
-    html = requests.get(url).content
+    html = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}).content
     all_paras = [para.text for para in justext.justext(html, justext.get_stoplist("English"))]
 
     with open('tmp', 'w') as fh:
